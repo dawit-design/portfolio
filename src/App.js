f8c6df5b00e12930a7e217252e2df575f7228fb9
Bryan Molk <brybrym92@MacBook-Pro-3.local>
2021-04-09 15:21:00 -0400

    home page created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import Navbar from './components/Navbar'
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import particles from 'react-particles-js'
+import Header from './components/Header'
+import Particles from 'react-particles-js';
 function App() {
   return (
-    <div className="App">
+    <>
+      <Particles
+        className='particles-canvas'
+        params={{
+          particles: {
+            number: {
+              value: 30,
+            density: {
+              enabled: true,
+              value_area: 900
+            }
+          },
+            shape: {
+              type: 'star',
+              stroke: {
+                width: 6,
+                color: '#f9ab00'
+              }
+            }
+          }
+      }}
+      />
       <Navbar />
-      <h2>Portfolio</h2>
-    </div>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Typed from 'react-typed'
+
+export default function Header() {
+  return (
+    <div className='header-wrapper'>
+      <div className='main-info'>
+        <h1>Software Engineering Immersive Graduate</h1>
+        <Typed
+          className='typed-text'
+          strings={["Web Design", "Web Development", "Graphic Design"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop={true}
+        />
+        <a href="https://drive.google.com/file/d/1GZZfHY3NdgntSbUaFuWp8TtXqQYSukwN/view?usp=sharing" className="btn-cv-show">Resume</a>
+      </div>
+      
+    </div>
+  )
+}
