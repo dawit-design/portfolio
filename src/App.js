e18eedf309bd10c1009594844bbaa12ecdb32715
Bryan Molk <brybrym92@MacBook-Pro-3.local>
2021-04-09 13:31:18 -0400

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Navbar from './components/Navbar'
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h2>Portfolio</h2>
     </div>
   );
