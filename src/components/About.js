564433f25b9947532c3bd6979dd8359ba661a97b
Bryan Molk <brybrym92@MacBook-Pro-3.local>
2021-04-10 10:00:21 -0400

    profile picture fix

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import dawit from '../dawit.jpg'
+import proImg from '../proImg.jpg'
 export default function About() {
   return (
     <div id="about" className="about-dev">
       <div className="about-me-intro">About Me</div>
       <div className="about-card-wrapper">
         <div className="about-card">
-          <img className="circular--landscape" src={dawit} alt="profile-image" />
+          <img className="circular--landscape" src={proImg} alt="profile-image" />
           <h1 className="fullname">Dawit Awegchow</h1>
           <p className="job">Web Developer</p>
           <p className="about-me">"An enthusiastic software engineer experienced in object-oriented programming languages including Ruby and JavaScript based programming, with a background of graphic design, sales and service industries, with strong teamwork skills from working in diverse teams of professionals. A passionate learner who is excited to help innovative, solve problems and build quality software and an eagerness to strengthen and learn new skills. With an emphasis in team-oriented and collaborative careers and activities, I excel at developing strong relationships with clients and delivering client needs in a timely."</p>
