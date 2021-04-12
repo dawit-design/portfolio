7b48546c8c593d2bcd2c85481f62efb51773c81b
Bryan Molk <brybrym92@MacBook-Pro-3.local>
2021-04-12 18:59:23 -0400

    contact form edit done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,31 +2,38 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div id="contact">
-      <section className="contact">
-        <div className="container">
-          <h1 className="contact-intro">Contact Me</h1>
-          <div className="row">
-            <div className="col-md-6">
-            <form action="https://getform.io/f/049dc321-e767-41dd-847b-66fba12939ed" method="POST">
-                  <input type="text" name="name" />
-                  <input type="email" name="email" />
-                  <textarea type="text" name="message" />
-                  <button type="submit">Send</button>
-                </form>
-                
-            </div>
-            <div className="col-md-6 contact-info">
-              <div className="social-icons"><b>Adress:</b><i className="fas fa-map-marker-alt"></i> Somerville, MA</div>
-              <div className="social-icons"><b>Phone:</b><i className="fas fa-phone"></i> +1 201 995 3890</div>
-              <div className="social-icons"><b>Email:</b><i className="fas fa-envelope"></i> dawitawegchow@gmail.com</div>
-              <div className="social-icons"><label><b>Get Social:</b></label></div>
-              <a className="social-icons" href="https://github.com/dawit-design"><i class="fab fa-github"></i></a>
-              <a className="social-icons" href="https://www.linkedin.com/in/dawit-endaylalu-0ab8a41b3/"><i className="fa fa-linkedin"></i></a>
-            </div>
-          </div>
+
+    <div className="contact-form">
+      <div>
+        <h1 className="contact-intro">Contact Me</h1>
+      </div>
+      <form action="https://getform.io/f/049dc321-e767-41dd-847b-66fba12939ed" method="POST">
+        <div>
+          <label>Name</label> <br/>
+          <input className="contact-form-input" type="text" name="name" required />
         </div>
-      </section>
+        <div>
+          <label >Email</label> <br/>
+          <input className="contact-form-input" type="email" name="email" required />
+        </div>
+        <div>
+          <label >Phone Number</label> <br/>
+          <input className="contact-form-input" type="number" name="phone-number" required />
+        </div>
+        <div>
+          <label >Message</label> <br/>
+          <textarea className="contact-form-input" type="text" name="message" required />
+        </div>
+        <div className="contact-button-div">
+        <button className="form-button" type="submit">Send</button>
+        </div>
+      </form>
+      
+      <div className="social">
+        <a className="social-icon" href="https://github.com/dawit-design"><i class="fab fa-github"></i></a>
+        <a className="social-icon" href="https://www.linkedin.com/in/dawit-endaylalu-0ab8a41b3/"><i className="fa fa-linkedin"></i></a>
+      </div>
     </div>
+
   )
 }
